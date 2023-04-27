ebc010677aab9003717e2fdf2ece0ec5c6cb5e7f
WoutThiers <wout.thiers@student.kuleuven.be>
2023-04-27 16:07:07 +0200

    begonnen met monster

--- src/RPG/Monster.java
+++ src/RPG/Monster.java
@@ -1,7 +1,17 @@
 package RPG;
 
 public class Monster extends Creature{
+    /**
+     * variable stating how much protection the monster has, how easily he can dodge or deflect attacks.
+     */
+    protected Armor naturalProtection;
+    /**
+     * Variable stating how much damage the monster does.
+     */
+    protected Weapon damage;
+
+
+
 
 
 }
- */
